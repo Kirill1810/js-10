5576e28fde61b81ae402bbce71d97e3832581dd7
Kirill1810 <129986639+Kirill1810@users.noreply.github.com>
2024-12-17 22:59:42 +0300

    апр

--- sj.js
+++ sj.js
@@ -53,3 +53,27 @@
       const callback = 0;
       
 
+
+
+
+
+
+
+
+
+
+      const calculateDiscountedPrice = (price, discount, callback) => {
+
+          const a = discount / 100;
+          const b = 1 - a;
+
+          callback = price * b;
+
+          console.log(callback);
+        };
+       const showDiscountedPrice = (message) => alert(`Знижка скдадає ${message}`);
+
+        
+       calculateDiscountedPrice(100, 10, callback);
+       
+       showDiscountedPrice(calculateDiscountedPrice);
